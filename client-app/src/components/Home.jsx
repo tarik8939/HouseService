b2d3769d3033c5f0c71fb97438e7a8380f64c9d9
tarik8939 <39026353+tarik8939@users.noreply.github.com>
2021-06-14 16:32:31 +0300

    Merge branch 'taras' into ilya

--- client-app/src/components/Home.jsx
+++ client-app/src/components/Home.jsx
@@ -3,25 +3,25 @@ import axios from 'axios';
 export class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
   componentDidMount() {
     this.load();
   }
-  load(){
-    axios.get('https://localhost:44307/api/user',  {
+  load() {
+    axios.get('https://localhost:44307/api/user', {
       headers: {
         'Content-Type': 'application/json'
       },
       withCredentials: true,
     }).then((response) => {
       console.log(response);
-  });
-    
+    });
+
   }
-  render() { 
-    return ( <h1>Hello</h1> );
+  render() {
+    return (<h1>Hello</h1>);
   }
 }
- 
+
 export default Home;
--- client-app/src/components/Nav.jsx
+++ client-app/src/components/Nav.jsx
@@ -3,25 +3,28 @@ import { Link } from 'react-router-dom';
 export class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
-  render() { 
-    return ( 
+  render() {
+    return (
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <Link to="/" className="navbar-brand">Home</Link>
-      <div>
-        <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
+        <div >
+          <Link to="/" className="navbar-brand">Home</Link>
+        </div>
+        <div className="col-md-10"></div>
+        <div className="col-md-1">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
               <Link to="/login" className="nav-link">Login</Link>
             </li>
             <li className="nav-item active">
               <Link to="/register" className="nav-link">Register</Link>
             </li>
-        </ul>
-      </div>
-    </nav>
-     );
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
- 
+
 export default Nav;
