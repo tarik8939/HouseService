18ef059cf9cda67486ddc40647a5a0cd1a13128f
Ілля Вовченко <Illia.Vovchenko.PZ.2018@edu.lpnu.ua>
2021-06-29 19:59:50 +0300

    adaptive

--- client-app/src/Advertisement/components/Ad/AdComponent.jsx
+++ client-app/src/Advertisement/components/Ad/AdComponent.jsx
@@ -19,12 +19,7 @@ function AdComponent(props) {
         )
       }
     }
-    // else {
-    //   return (
-    //     <div className="col-md-3 col-xs-3 col-lg-3 col-sm-3"></div>
-    //     <label className="col-md-3 col-xs-3 col-lg-3 col-sm-3">                     </label>
-    //   )
-    // }
+    
   }
   const renderStatus = (item) => {
     if (item.status != null) {
@@ -34,67 +29,10 @@ function AdComponent(props) {
     }
   }
 
-
-  //return (
-  // <div className="border border-primary rounded m-1 ml-4 container-fluid" key={props.key}>
-  //   <div className="row mt-2">
-  //     <span className="col-md-10 col-xs-10 col-lg-10 col-sm-10"><LinesEllipsis
-  //       text={data.name}
-  //       maxLine='1'
-  //       ellipsis='...'
-  //       basedOn='letters'
-  //       className="h3"
-  //     /></span>
-  //     {/* <span className="col-2"></span> */}
-  //     <span className="text-success fs-2 col-md-1 col-xs-1 col-lg-1 col-sm-1 ml-5">{data.price}$</span>
-  //   </div>
-  //   <div className="row mt-1 mb-2">
-  //     <LinesEllipsis
-  //       text={data.description}
-  //       maxLine='2'
-  //       ellipsis='...'
-  //       basedOn='words'
-  //       className="col-md-10 col-xs-10 col-lg-10 col-sm-10"
-  //     />
-  //   </div>
-  //   <div className="row mt-1 mb-1">
-  //     <span className="text-primary col-md-2 col-xs-2 col-lg-2 col-sm-2 mt-1 ">{data.user.fullName}</span>
-  //     <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 mt-1">From: {moment(data.startDate).format('L')}</span>
-  //     <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 mt-1">Until: {moment(data.endDate).format('L')}</span>
-  //     <span className="col-md-3 col-xs-3 col-lg-3 col-sm-3">
-  //       {renderStatus(data)}
-  //     </span>
-  //     <div className="col-md-12 col-xs-12 col-lg-3 col-sm-12">
-  //       {/* <div className="row">
-  //         <div className="col-md-6 col-xs-6 col-lg-6 col-sm-6">
-  //           {renderBtn(data)}
-  //         </div>
-  //         <div className="col-md-6 col-xs-6 col-lg-6 col-sm-6">
-  //           <Link to={`/AdView/${data.advertisementID}`} className="btn btn-primary text-center">
-  //             Details
-  //           </Link>
-  //         </div>
-  //       </div> */}
-  //       <div className="btn-toolbar">
-
-  //         <span >
-  //           {renderBtn(data)}
-  //         </span>
-  //         <div className="ml-2">
-  //           <Link to={`/AdView/${data.advertisementID}`} className="btn btn-primary text-center">
-  //             Details
-  //           </Link>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
-  //)
-
   return (
-    <div className="border border-primary rounded m-1 ml-4 container" key={props.key}>
+    <div className="border border-primary rounded m-1 container-fluid" key={props.key}>
       <div className="row mt-2">
-        <span className="col-md-10 col-xs-10 col-lg-10 col-sm-10"><LinesEllipsis
+        <span className="col-lg-10 col-11 "><LinesEllipsis
           text={data.name}
           maxLine='1'
           ellipsis='...'
@@ -102,7 +40,7 @@ function AdComponent(props) {
           className="h3"
         /></span>
         {/* <span className="col-2"></span> */}
-        <span className="text-success fs-2 col-md-1 col-xs-1 col-lg-1 col-sm-1">{data.price}$</span>
+        <span className="text-success fs-3 col-12 col-lg-1 offset-lg-1">{data.price}$</span>
       </div>
       <div className="row mt-1 mb-2">
         <LinesEllipsis
@@ -110,28 +48,28 @@ function AdComponent(props) {
           maxLine='2'
           ellipsis='...'
           basedOn='words'
-          className="col-md-10 col-xs-10 col-lg-10 col-sm-10"
+          className="col-md-10 col-10 col-lg-10 col-sm-10 col-xl-10  lead"
         />
       </div>
       <div className="row mt-2 mb-1">
-        <span className="text-primary col-md-2 col-xs-2 col-lg-2 col-sm-2 ">{data.user.fullName}</span>
-        <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 ">From: {moment(data.startDate).format('L')}</span>
-        <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 ">Until: {moment(data.endDate).format('L')}</span>
-        <span className="col-md-3 col-xs-3 col-lg-3 col-sm-3">
+        <span className="text-primary col-md-2 col-xs-2 col-lg-2 col-sm-2 col-xl-2 fs-6">{data.user.fullName}</span>
+        <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 col-xl-2 fs-6">From: {moment(data.startDate).format('L')}</span>
+        <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 col-xl-2 fs-6">Until: {moment(data.endDate).format('L')}</span>
+        <span className="col-md-4 col-4 col-lg-4 col-sm-4 col-xl-2 fs-6">
           {renderStatus(data)}
         </span>
-        <span className="col-md-2 col-xs-2 col-lg-2 col-sm-2 ">
+        <div className="col-md-12 col-12 col-lg-3 col-sm-12 col-xl-3 offset-xl-1">
           <div className="row">
-            <div className="col-md-6 col-xs-6 col-lg-6 col-sm-6 ">
+            <div className="col-md-6 col-6 col-lg-4 col-sm-6 col-xl-5">
               {renderBtn(data)}
             </div>
-            <div className="col-md-6 col-xs-6 col-lg-6 col-sm-6 ">
+            <div className="col-md-6 col-6 col-lg-4 col-sm-6 col-xl-6">
               <Link to={`/AdView/${data.advertisementID}`} className="btn btn-primary text-center">
                 Details
               </Link>
             </div>
           </div>
-        </span>
+        </div>
       </div>
     </div>
   )
--- client-app/src/Advertisement/pages/ListOfAds/ListOfAdsComponent.jsx
+++ client-app/src/Advertisement/pages/ListOfAds/ListOfAdsComponent.jsx
@@ -5,38 +5,37 @@ function ListOfAdsComponent(props) {
   const RenderAds = () => {
 
     return (
-      <div className="row">
-        <div className="col-md-1 col-xs-1 col-lg-1 col-sm-1 "></div>
-        <div className="col-md-10 col-xs-10 col-lg-10 col-sm-10 ">
+      <div>
           {props.state.Ads.filter(ad => ad.name.toLowerCase().includes(props.state.title.toLowerCase())).map((item) => (
             <Ad key={item.advertisementID} dataParent={item} load={props.load} />
           ))}
-        </div>
       </div>
     )
   }
 
   return (
-    <div className="row">
-      <div className="col-md-1 "></div>
-      <div className="col-md-10 ">
-        <label className=" mr-2">Search by title </label>
-        <input type="text" name="title"
-          className="col-md-6  rounded ml-1 mr-2"
+    <div>
+    <div className="row mb-3">
+        <p className="col-auto fs-4 text-right">Search</p>
+        <input type="search" name="title"
+          className="col-9  col-lg-10 col-xl-11"
           value={props.state.title}
           onChange={(e) => props.HandleValueChange(e)}>
         </input>
-        <label className="ml-5 mr-2">Sort by </label>
-        <select className="col-md-3 rounded ml-1" onChange={(e) => props.onSort(e)}>
+    </div>
+    <div className="row mb-3">
+        <p className="col-auto fs-4 text-right">Sort by </p>
+        <select className="col-9  col-lg-10 col-xxl-11" onChange={(e) => props.onSort(e)}>
           <option value="Select parametr for sort" defaultValue >Select parametr for sort </option>
           <option value="Name">Name</option>
           <option value="Price">Price</option>
           <option value="Date">Date</option>
         </select>
-        <p>{props.state.Ads.filter(ad => ad.name.toLowerCase().includes(props.state.title.toLowerCase())).length} searched results</p>
-      </div>
-      <div className="col-md-3 "></div>
-      {RenderAds()}
+    </div>
+    <div className="row mb-3">
+        <p className="col-auto fs-4 text-right">{props.state.Ads.filter(ad => ad.name.toLowerCase().includes(props.state.title.toLowerCase())).length} searched results:</p>
+    </div>
+    <div>{RenderAds()}</div>
     </div>
   );
 }
